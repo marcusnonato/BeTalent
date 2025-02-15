31406ddaa387f9dda4a1106d61160a2040f497ef
Marcus Nonato <marcusononato@gmail.com>
2025-02-15 20:32:05 -0300

    feat: add icons images to project

---
+++ app/assets/images.ts
@@ -0,0 +1,6 @@
+export const searchIcon = require('./search-bar-icon.png');
+export const blackSearchIcon = require('./black-search-bar-icon.png');
+export const chevronDownIcon = require('./chevron-down.png');
+export const blueChevronDownIcon = require('./blue-chevron-down.png');
+export const chevronUpIcon = require('./chevron-up.png');
+export const circle = require('./circle.png');
