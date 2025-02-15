c143a29508f47304e1c287cbcd46b501980e6588
Marcus Nonato <marcusononato@gmail.com>
2025-02-15 20:33:30 -0300

    feat: add globalTheme file

---
+++ app/themes/globalTheme.ts
@@ -0,0 +1,15 @@
+export const colors = {
+  primary: '#0500FF',
+  black_neural: '#1C1C1C',
+  gray: {
+    100: '#F0F0F0',
+    200: '#F5F5F5',
+    300: '#DFDFDF',
+    400: '#9E9E9E',
+  },
+};
+
+export const fonts = {
+  normal: 'Helvetica',
+  bold: 'Helvetica-Bold',
+};
